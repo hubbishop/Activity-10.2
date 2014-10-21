7fa2f7985c0273e711bd98b3a26a9b5d055e05ec
hubbishop <icb4@alfred.edu>
2014-10-21 14:48:02 -0400

    to the cloud

---
+++ Activity-10.2.py
@@ -0,0 +1,10 @@
+__author__ = 'Dark-Knight'
+
+m = [8, 2, 4, 3, 6, 5, 7, 1]
+print(m)
+for i in range(len(m)-1):
+    if m[i] > m[i+1]:
+        temp = m[i]
+        m[i] = m[i+1]
+        m[i+1] = temp
+print(m)
